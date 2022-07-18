c92a7dbfb2160fe09804fc5f7356647e22e944f7
Alexandra Eydinova <alexandra.eydinova@gmail.com>
2022-07-18 22:39:22 +0100

    Bonigarcia setup browsers pretty well.

--- src/test/java/stellar/burger/AllTestsInit.java
+++ src/test/java/stellar/burger/AllTestsInit.java
@@ -1,17 +1,45 @@
 package stellar.burger;
 
+import com.codeborne.selenide.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
 import stellar.burgers.pages.HomePage;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class AllTestsInit {
-
+    public static final String FIREFOX_BROWSER_NAME = "firefox";
+    public static final String CHROME_BROWSER_NAME = "chrome";
+    private static Log log = LogFactory.getLog(AllTestsInit.class);
     public static HomePage homePage;
+    public static WebDriver driver;
 
     @BeforeAll
     public static void setup() {
+        setBrowserFromProperty();
         homePage = open(HomePage.URL, HomePage.class);
+    }
+
+    public static void setBrowserFromProperty(){
+        //ToDo Take the browser from the properties.
+        Configuration.browser = FIREFOX_BROWSER_NAME;
+        if(Configuration.browser == FIREFOX_BROWSER_NAME) {
+            //Default variant works well here.
+        } else if(Configuration.browser == CHROME_BROWSER_NAME){
+            //Default variant works well here.
+        } else {
+            log.error("Provided through the property browser " + Configuration.browser + " is unknown.");
+        }
+    }
+
+    @AfterAll
+    public static void quitBrowser() {
+        if(driver != null) {
+            driver.quit();
+        }
     }
 
 }
--- src/test/java/stellar/burger/LoginPageVerificationTests.java
+++ src/test/java/stellar/burger/LoginPageVerificationTests.java
@@ -1,15 +1,14 @@
-package stellar.burger.tests.loginLogout;
+package stellar.burger;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import stellar.burger.AllTestsInit;
 import stellar.burgers.pages.HomePage;
 import stellar.burgers.pages.LoginPage;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static com.codeborne.selenide.Selenide.page;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LoginPageTests extends AllTestsInit {
+public class LoginPageVerificationTests extends AllTestsInit {
 
     public static final String SHORT_VALUE_FOR_PASSWORD_TEXT = "short";
     public static final String INCORRECT_PASSWORD_ERROR_MESSAGE = "Некорректный пароль";
